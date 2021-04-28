6a1d08c4884a648f0dc7a3e5d5ac53bc186fe6f9
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-04-28 21:46:12 +0300

    create: added routing

---
+++ src/pages/HomePage/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='main'>
+            <div className='container'>
+                <h1>Photify</h1>
+                <h2>Free online development and editing tool that will satisfy all your photo and graphic needs!</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
