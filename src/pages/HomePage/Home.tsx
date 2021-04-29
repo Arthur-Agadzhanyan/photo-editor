d3fdb5ecad8b8e33fc77658da4b6c20186be1888
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-04-29 19:12:05 +0300

    create: added homepage styles

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+const mainStyle = {
+    background: `url(/main-bg.jpg)`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+}
 
 const Home = () => {
     return (
-        <div className='main'>
+        <div className='main' style={mainStyle}>
             <div className='container'>
-                <h1>Photify</h1>
-                <h2>Free online development and editing tool that will satisfy all your photo and graphic needs!</h2>
+                <h1 className='main__title'>Photify</h1>
+                <h2 className='main__subtitle'>Free online development and editing tool that will satisfy all your photo and graphic needs!</h2>
+                <Link to='/editor' className='main__link'>Start editing</Link>
             </div>
         </div>
     );
