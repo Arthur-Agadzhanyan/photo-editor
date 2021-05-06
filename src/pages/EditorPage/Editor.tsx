92b6fc27e7012e063ae8aeb4911dcc200589950a
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-05-06 20:44:22 +0300

    change: DEFAULT_OPTIONS moved to constants.ts file

--- src/pages/EditorPage/Editor.tsx
+++ src/pages/EditorPage/Editor.tsx
@@ -5,80 +5,8 @@ import RightBar from '../../components/RightBar';
 import SelectImagePage from '../../components/SelectImagePage';
 import Sidebar from '../../components/Sidebar';
 import Slider from '../../components/Slider'
+import { DEFAULT_OPTIONS } from '../../constants';
 import { OPTION } from '../../interfaces';
-
-const DEFAULT_OPTIONS: OPTION[] = [
-  {
-    name: 'Brightness',
-    property: 'brightness',
-    value: 100,
-    range: {
-      min: 0,
-      max: 200
-    },
-    unit: '%'
-  },
-  {
-    name: 'Contrast',
-    property: 'contrast',
-    value: 100,
-    range: {
-      min: 0,
-      max: 200
-    },
-    unit: '%'
-  },
-  {
-    name: 'Saturation',
-    property: 'saturate',
-    value: 100,
-    range: {
-      min: 0,
-      max: 200
-    },
-    unit: '%'
-  },
-  {
-    name: 'Grayscale',
-    property: 'grayscale',
-    value: 0,
-    range: {
-      min: 0,
-      max: 200
-    },
-    unit: '%'
-  },
-  {
-    name: 'Sepia',
-    property: 'sepia',
-    value: 0,
-    range: {
-      min: 0,
-      max: 200
-    },
-    unit: '%'
-  },
-  {
-    name: 'Hue Rotate',
-    property: 'hue-rotate',
-    value: 0,
-    range: {
-      min: 0,
-      max: 360
-    },
-    unit: 'deg'
-  },
-  {
-    name: 'Blur',
-    property: 'blur',
-    value: 0,
-    range: {
-      min: 0,
-      max: 20
-    },
-    unit: 'px'
-  }
-]
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
